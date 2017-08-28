49f4e2e9cdebdd53a240d799706d40f33f80ccef
Steve Hu <stevehu@gmail.com>
2017-08-28 10:55:34 -0400

    fixes #10 update test case for api-certification

--- api-certification/src/test/java/com/networknt/portal/certification/handler/ServerInfoProcessorTest.java
+++ api-certification/src/test/java/com/networknt/portal/certification/handler/ServerInfoProcessorTest.java
@@ -6,7 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 public class ServerInfoProcessorTest {
-    @Test
+    /**
+     * This test needs petstore from light-example-4j/rest/petstore to be up and running.
+     * @throws Exception
+     */
+    //@Test
     public void testProcess() throws Exception {
         Map<String, Object> map = ServerInfoRetriever.retrieve("https://localhost:8443", "/v2/server/info", null);
         List list = ServerInfoProcessor.process(map);
--- api-certification/src/test/java/com/networknt/portal/certification/handler/ServerInfoRetrieverTest.java
+++ api-certification/src/test/java/com/networknt/portal/certification/handler/ServerInfoRetrieverTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.Map;
 
 public class ServerInfoRetrieverTest {
-    @Test
+    //@Test
     public void testRetrieveServerInfo() throws Exception {
         Map<String, Object> map = ServerInfoRetriever.retrieve("https://localhost:8443", "/v2/server/info", null);
         // make sure the the map is server info object instead of status object that indicates an error.
