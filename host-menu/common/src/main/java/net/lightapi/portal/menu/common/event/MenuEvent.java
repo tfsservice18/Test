d4463af6cd9f4447d2eaa01807b18bb75a58c6db
Steve Hu <stevehu@gmail.com>
2017-09-28 11:48:48 -0400

    events subscribe the right aggregate package now

--- host-menu/common/src/main/java/net/lightapi/portal/menu/common/event/MenuEvent.java
+++ host-menu/common/src/main/java/net/lightapi/portal/menu/common/event/MenuEvent.java
@@ -3,6 +3,6 @@ package net.lightapi.portal.menu.common.event;
 import com.networknt.eventuate.common.Event;
 import com.networknt.eventuate.common.EventEntity;
 
-@EventEntity(entity = "com.networknt.portal.menu.MenuAggregate")
+@EventEntity(entity = "net.lightapi.portal.menu.domain.MenuAggregate")
 public interface MenuEvent extends Event {
 }
--- host-menu/common/src/main/java/net/lightapi/portal/menu/common/event/MenuItemEvent.java
+++ host-menu/common/src/main/java/net/lightapi/portal/menu/common/event/MenuItemEvent.java
@@ -3,6 +3,6 @@ package net.lightapi.portal.menu.common.event;
 import com.networknt.eventuate.common.Event;
 import com.networknt.eventuate.common.EventEntity;
 
-@EventEntity(entity = "com.networknt.portal.menu.MenuItemAggregate")
+@EventEntity(entity = "net.lightapi.portal.menu.domain.MenuItemAggregate")
 public interface MenuItemEvent extends Event {
 }
