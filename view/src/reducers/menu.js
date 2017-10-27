8c5ed3dcfad22d2565b15f300bc8aea343f61dee
Dinesh Alapati <dine.alapati@gmail.com>
2017-10-27 16:29:35 -0400

    chore: add initial state for menu reducer

--- view/src/reducers/menu.js
+++ view/src/reducers/menu.js
@@ -1,7 +1,12 @@
 /* eslint no-underscore-dangle: 0 */
 import { MENU_SERVICE_SUCCESS } from '../constants';
 
-export default function runtime(state = {}, action) {
+const initialState = {
+  name: '',
+  routes: [],
+};
+
+export default function runtime(state = initialState, action) {
   switch (action.type) {
     case MENU_SERVICE_SUCCESS:
       return {
