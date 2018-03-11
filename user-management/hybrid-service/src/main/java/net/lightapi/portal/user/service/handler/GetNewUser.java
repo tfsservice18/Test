144e74406f642b518b704643e3dae64f88ff41df
chenyan71 <passWord11!!>
2018-03-11 17:33:51 -0400

    change the test port for build issue

--- user-management/hybrid-service/src/main/java/net/lightapi/portal/user/service/handler/GetNewUser.java
+++ user-management/hybrid-service/src/main/java/net/lightapi/portal/user/service/handler/GetNewUser.java
@@ -1,15 +1,54 @@
 
 package net.lightapi.portal.user.service.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.portal.usermanagement.model.auth.service.UserService;
+import com.networknt.portal.usermanagement.model.auth.service.UserServiceImpl;
+import com.networknt.portal.usermanagement.model.common.crypto.PasswordSecurity;
+import com.networknt.portal.usermanagement.model.common.domain.UserDto;
+import com.networknt.portal.usermanagement.model.common.model.user.ConfirmationToken;
+import com.networknt.portal.usermanagement.model.common.model.user.User;
+import com.networknt.portal.usermanagement.model.common.model.user.UserRepository;
+import com.networknt.service.SingletonServiceFactory;
 import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.ByteBuffer;
+import java.util.Optional;
 
 @ServiceHandler(id="lightapi.net/user/createUser/0.1.0")
 public class GetNewUser implements Handler {
+    static final Logger logger = LoggerFactory.getLogger(GetNewUser.class);
+    private UserRepository userRepository = (UserRepository) SingletonServiceFactory.getBean(UserRepository.class);
+    private static PasswordSecurity passwordSecurity = (PasswordSecurity)SingletonServiceFactory.getBean(PasswordSecurity.class);
+    private UserService service = new UserServiceImpl(passwordSecurity, null, userRepository);
     @Override
     public ByteBuffer handle(Object input)  {
-        return NioUtils.toByteBuffer("");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String result = "Ok!";
+        try {
+            String json = mapper.writeValueAsString(input);
+            UserDto userDto = mapper.readValue(json, UserDto.class);
+
+            User user = service.fromUserDto(userDto);
+            service.signup(user, userDto.getPassword(), false);
+
+            //TODO remove the following implemetation after confirm email implemented
+            Optional<ConfirmationToken> token = user.getConfirmationTokens().stream().findFirst();
+            if (token.isPresent()) {
+                result = "http://localhost:8080/v1/user/token/" + token.get().getId();
+            }
+
+        } catch (Exception e) {
+            result = e.getMessage();
+            //TODO handler Exception, add log info?
+        }
+
+        return NioUtils.toByteBuffer(result);
     }
 }
--- user-management/hybrid-service/src/test/java/net/lightapi/portal/user/service/handler/GetNewUserTest.java
+++ user-management/hybrid-service/src/test/java/net/lightapi/portal/user/service/handler/GetNewUserTest.java
@@ -4,12 +4,14 @@ package net.lightapi.portal.user.service.handler;
 import com.networknt.client.Http2Client;
 import com.networknt.exception.ApiException;
 import com.networknt.exception.ClientException;
+import com.networknt.service.SingletonServiceFactory;
 import io.undertow.UndertowOptions;
 import io.undertow.client.ClientConnection;
 import io.undertow.client.ClientRequest;
 import io.undertow.client.ClientResponse;
 import io.undertow.util.Headers;
 import io.undertow.util.Methods;
+import org.h2.tools.RunScript;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -18,7 +20,12 @@ import org.slf4j.LoggerFactory;
 import org.xnio.IoUtils;
 import org.xnio.OptionMap;
 
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -32,10 +39,31 @@ public class GetNewUserTest {
     static final int httpPort = server.getServerConfig().getHttpPort();
     static final int httpsPort = server.getServerConfig().getHttpsPort();
     static final String url = enableHttp2 || enableHttps ? "https://localhost:" + httpsPort : "http://localhost:" + httpPort;
+/*
+    public static DataSource ds;
+    static {
+        ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
+        try (Connection connection = ds.getConnection()) {
+            // Runscript doesn't work need to execute batch here.
+            String schemaResourceName = "/user_management_ddl.sql";
+            InputStream in = GetNewUserTest.class.getResourceAsStream(schemaResourceName);
 
+            if (in == null) {
+                throw new RuntimeException("Failed to load resource: " + schemaResourceName);
+            }
+            InputStreamReader reader = new InputStreamReader(in);
+            RunScript.execute(connection, reader);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static final String s = "{\"host\":\"lightapi.net\",\"service\":\"menu\",\"action\":\"createMenu\",\"version\":\"0.1.0\",\"data\":{\"host\":\"example.org\",\"description\":\"example org web site\",\"contains\":[\"1\",\"2\",\"3\"]}}";
+*/
     @Test
     public void testGetNewUser() throws ClientException, ApiException {
-        /*
+/*
         final Http2Client client = Http2Client.getInstance();
         final CountDownLatch latch = new CountDownLatch(1);
         final ClientConnection connection;
@@ -45,11 +73,13 @@ public class GetNewUserTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        System.out.println("\n");
+        System.out.println("json:" + s);
         try {
             ClientRequest request = new ClientRequest().setPath("/api/json").setMethod(Methods.POST);
             request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
             request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
-            connection.sendRequest(request, client.createClientCallback(reference, latch, "request body to be replaced"));
+            connection.sendRequest(request, client.createClientCallback(reference, latch, s));
             latch.await();
         } catch (Exception e) {
             logger.error("Exception: ", e);
@@ -61,6 +91,6 @@ public class GetNewUserTest {
         String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
         Assert.assertEquals(200, statusCode);
         Assert.assertNotNull(body);
-        */
+*/
     }
 }
